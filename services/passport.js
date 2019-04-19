37c1c087e3969dbdfb78b96d8191e6da1c7c0702
Andris Smolovskis <smolovskis@gmail.com>
2019-04-19 08:31:55 +0300

    - Debug

--- services/passport.js
+++ services/passport.js
@@ -33,6 +33,7 @@ passport.use(
                 return done(null, existingUser);
             }
             const user = await new User({googleId: profile.id}).save();
+            console.log('User found: ' + user);
             done(null, user);
         }
     )
